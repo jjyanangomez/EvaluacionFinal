58901e3691dc4b28288f60fb862e47b6b1e6c3e5
jjyanangomez <juanyasa_04@hotmail.com>
2024-02-24 10:18:11 -0500

    Evaluacion final completada

---
+++ notas.js
@@ -0,0 +1,82 @@
+let estudiantes = [
+    {nombre:"Marcos",apellido:"Erazo",nota1:8.4,nota2:9.5,nota3:8.7,total:26.6,promedio:8.7},
+    {nombre:"Maritza",apellido:"Rosero",nota1:5.4,nota2:8.5,nota3:9.7,total:23.6,promedio:9.7},
+    {nombre:"Esteban",apellido:"Guaranda",nota1:9.4,nota2:10.0,nota3:9.0,total:28.4,promedio:9},
+    {nombre:"Ricardo",apellido:"Batista",nota1:6.4,nota2:9.5,nota3:8.9,total:24.8,promedio:8.9},
+]
+desactivarBoton =function(){
+    deshabilitarComponente("btnGuardar");
+}
+calcularTotal = function(n1,n2,n3){
+    return (n1+n2+n3);
+}
+calcularPromedio = function(p1,p2,p3){
+    let promedio = (p1+p2+p3)/3
+    return promedio;
+}
+calcular = function(){
+    let valueNote = recuperarFloat("txtNota1");
+    let valueNote2 = recuperarFloat("txtNota2");
+    let valueNote3 = recuperarFloat("txtNota3");
+    let resultTotal = calcularTotal(valueNote,valueNote2,valueNote3);
+    mostrarTexto("lblTotal",resultTotal);
+    let resultPromedio = calcularPromedio(valueNote,valueNote2,valueNote3);
+    mostrarTexto("lblPromedio",resultPromedio);
+    habilitarComponente("btnGuardar")
+}
+guardar = function(){
+    let valueName = recuperarTexto("txtNombre");
+    let valueName2 = recuperarTexto("txtApellido");
+    let valueNote = recuperarFloat("txtNota1");
+    let valueNote2 = recuperarFloat("txtNota2");
+    let valueNote3 = recuperarFloat("txtNota3");
+    let resultTotal = calcularTotal(valueNote,valueNote2,valueNote3);
+    let resultPromedio = calcularPromedio(valueNote,valueNote2,valueNote3);
+    let objStudent = {
+        nombre:valueName,
+        apellido:valueName2,
+        nota1:valueNote,
+        nota2:valueNote2,
+        nota3:valueNote3,
+        total:resultTotal,
+        promedio:resultPromedio
+    }
+    estudiantes.push(objStudent);
+    limpiar();
+    mostrarEstudiantes();
+}
+
+mostrarEstudiantes = function(){
+    let cmpTabla = document.getElementById("divTabla");
+    let contenidoTabla ="<table class='neon-table'><tr>"+
+    "<th>NOMBRE</th>"+
+    "<th>NOTA 1</th>"+
+    "<th>NOTA 2</th>"+
+    "<th>NOTA 3</th>"+
+    "<th>TOTAL</th>"+
+    "<th>PROMEDIO</th>"+
+    "</tr>";
+    let objPersona;
+    for(let i=0;i<estudiantes.length;i++){
+        objPersona = estudiantes[i];
+        contenidoTabla+=
+        "<tr>"
+        +"<td>"+objPersona.nombre+" "+objPersona.apellido+"</td>" 
+        +"<td>"+objPersona.nota1+"</td>"
+        +"<td>"+objPersona.nota2+"</td>"
+        +"<td>"+objPersona.nota3+"</td>"  
+        +"<td>"+objPersona.total+"</td>"    
+        +"<td>"+objPersona.promedio+"</td>"  
+        +"</tr>";
+    }
+    contenidoTabla +="</table>"
+    cmpTabla.innerHTML=contenidoTabla;
+    deshabilitarComponente("btnGuardar");
+}
+limpiar = function(){
+    mostrarTexto("txtNombre","");
+    mostrarTexto("txtApellido","");
+    mostrarTexto("txtNota1","");
+    mostrarTexto("txtNota2","");
+    mostrarTexto("txtNota3","");
+}
---
+++ utilitarios.js
@@ -0,0 +1,85 @@
+
+mostrarImagen=function(idComponente,rutaImagen){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.src =  rutaImagen;
+}
+mostrarTexto=function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.innerText = mensaje;
+}
+mostrarTextoEnCaja = function(idComponente,mensaje){
+    let componente;
+    componente=document.getElementById(idComponente);
+    componente.value = mensaje;
+}
+
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente=document.getElementById(idComponente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+}
+
+recuperarInt = function(idComponente){
+   let valorCaja= recuperarTexto(idComponente);
+   let valorEntero = parseInt(valorCaja);
+   return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let valorCaja= recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+ }
+
+mostrarComponente = function(idComponente){
+    document.getElementById(idComponente).style.display = "block";
+}
+
+ocultarComponente = function(idComponente){
+    document.getElementById(idComponente).style.display = "none";
+}
+
+deshabilitarComponente = function(idComponente){
+    document.getElementById(idComponente).disabled = true;
+}
+
+habilitarComponente = function(idComponente){
+    document.getElementById(idComponente).disabled = false;
+}
+esMayuscula = function (caracter) {
+    codigoAscii = caracter.charCodeAt(0);
+    if ((codigoAscii >= 65 && codigoAscii <= 90)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+esDigito = function (caracter) {
+    let valorCaracter = caracter.charCodeAt(0);
+    if (valorCaracter >= 48 && valorCaracter <= 57) {
+        return true;
+    } else {
+        return false;
+    }
+};
+recuperarTextoDiv = function (idComponente) {
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.textContent;
+    return valorIngresado;
+}
+recuperarIntDiv = function (idComponente) {
+    let valorDiv = recuperarTextoDiv(idComponente);
+    let valorEntero = parseInt(valorDiv);
+    return valorEntero;
+}
+recuperarFloatDiv = function (idComponente) {
+    let valorDiv = recuperarTextoDiv(idComponente);
+    let valorFlotante = parseFloat(valorDiv);
+    return valorFlotante;
+}
